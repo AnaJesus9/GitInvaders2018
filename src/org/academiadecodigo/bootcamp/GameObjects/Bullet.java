6fe1ece7d59b0d60d14d2f9538be932fed92268b
AJesus <beaasilveiira@gmail.com>
2018-10-07 22:20:02 +0000

    Import Position and some changes

--- src/org/academiadecodigo/bootcamp/GameObjects/Bullet.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/Bullet.java
@@ -3,6 +3,8 @@ package org.academiadecodigo.bootcamp.GameObjects;
 import org.academiadecodigo.bootcamp.Game;
 import org.academiadecodigo.bootcamp.GameEngine.Directions;
 
+import javax.swing.text.Position;
+
 public class Bullet extends Game {
 
     private int damage;
@@ -17,7 +19,7 @@ public class Bullet extends Game {
         this.target = target;
     }
 
-    public void move(Directions directions){
+    public void move(Directions direction){
         position.movePosition(direction, 1);
     }
     public void hit(int damage){
