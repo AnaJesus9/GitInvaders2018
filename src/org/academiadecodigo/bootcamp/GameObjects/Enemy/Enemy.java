b92dab6a3e1b454b098e38c5eecdf26d6bd76a44
AJesus <beaasilveiira@gmail.com>
2018-10-07 13:50:09 +0000

    Working on Enemy

--- src/org/academiadecodigo/bootcamp/GameObjects/Enemy/Enemy.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/Enemy/Enemy.java
@@ -1,4 +1,38 @@
 package org.academiadecodigo.bootcamp.GameObjects.Enemy;
 
-public class Enemy {
+import org.academiadecodigo.bootcamp.GameObjects.GameObjects;
+import org.academiadecodigo.bootcamp.GameObjects.Shootable;
+
+public abstract class Enemy extends GameObjects implements Shootable {
+
+    private Position position;
+    private int health;
+    private boolean destroyed;
+
+    public Enemy(Position position){
+        this.health = 100;
+        this.destroyed = false;
+        this.position = new Position();
+
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return false;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        this.destroyed = destroyed;
+    }
+
+
+
 }
