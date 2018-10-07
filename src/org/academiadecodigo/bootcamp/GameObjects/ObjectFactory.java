ae03072bf345df5a4eebd4761c6e83584dd2acd9
Jonathan <jonathan.ds.afonso@gmail.com>
2018-10-07 17:14:30 +0000

    initial object factory commit

--- src/org/academiadecodigo/bootcamp/GameObjects/ObjectFactory.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/ObjectFactory.java
@@ -1,4 +1,27 @@
 package org.academiadecodigo.bootcamp.GameObjects;
 
+import jdk.jshell.spi.ExecutionControl;
+import org.academiadecodigo.bootcamp.GameObjects.Enemy.Enemy;
+
 public class ObjectFactory {
+
+
+    public Enemy getNewEnemy() {
+
+        //What's the enemy method signature?
+        throw new ExecutionControl.NotImplementedException();
+
+    }
+
+    public Player getNewPlayer() {
+
+        throw new ExecutionControl.NotImplementedException();
+    }
+    
+    /*
+    public Boss getNewBoss() {
+
+
+     }
+      */
 }
