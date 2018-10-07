279c9cc066a590f590c7cfaadbe19605d7e2d5e9
Brusi <brusibeat@hotmail.com>
2018-10-07 19:55:32 +0000

    corrected an error in Shoot method

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -1,5 +1,30 @@
 package org.academiadecodigo.bootcamp;
+import org.academiadecodigo.bootcamp.GameObjects.*;
 
 public class Game {
+    private Player player;
+    private GameObjects[] gameObjects;
+    private final int NUMBER_OF_ENEMIES = 20;
 
+    public Game(){
+        init();
+
+        startGame();
+    }
+
+    public void init(){
+        this.player = new Player();
+        this.gameObjects = new GameObjects[NUMBER_OF_ENEMIES];
+        createGameObjects();
+    }
+
+    public void startGame(){
+
+    }
+
+    public void createGameObjects() {
+        for(int i = 0; i < NUMBER_OF_ENEMIES; i++){
+            this.gameObjects[i] = ObjectFactory.createNewEnemy();
+        }
+    }
 }
