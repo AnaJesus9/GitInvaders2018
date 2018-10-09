37ff0c9980bccf8f057a461fb966a58e17fbb28c
Ana Jesus <beaasilveiira@gmail.com>
2018-10-09 10:05:54 +0000

    Enemy dev changes

--- src/org/academiadecodigo/bootcamp/GameObjects/Enemy/Ship.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/Enemy/Ship.java
@@ -1,22 +1,22 @@
 package org.academiadecodigo.bootcamp.GameObjects.Enemy;
 
-import org.academiadecodigo.bootcamp.GameEngine.CreatObject;
+import org.academiadecodigo.bootcamp.GameEngine.CreateObject
 import org.academiadecodigo.bootcamp.GameEngine.Directions;
 import org.academiadecodigo.bootcamp.GameEngine.Position;
 import org.academiadecodigo.bootcamp.GameObjects.Bullet;
 
 public class Ship extends Enemy {
 
-    private CreatObject object;
+    private CreateObject object;
 
     public Ship( Position position){
         super(position);
-        this.object = new CreatObject(position.getRow(), position.getCol());
+        this.object = new CreateObject(position.getRow(), position.getCol());
     }
 
     @Override
     public void move(Directions direction) {
-        getPosition().movePosition(direction, 1);
+        getPosition().move(direction, 1);
         object.move(direction, 1);
     }
 
