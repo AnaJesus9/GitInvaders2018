7b3fd763c628330734dae211318c906fe3321a5f
Jonathan <jonathan.ds.afonso@gmail.com>
2018-10-12 01:03:53 +0000

    simple menu impl.

--- src/org/academiadecodigo/bootcamp/GameEngine/Menu/Menu.java
+++ src/org/academiadecodigo/bootcamp/GameEngine/Menu/Menu.java
@@ -15,20 +15,18 @@ public class Menu implements Grid, KeyboardHandler {
 
     //private Game game;
     private Picture menu;
-    private Picture[] pictures;
     private int row;
     private int col;
     private int CELLSIZE = GameConfigs.CELLSIZE;
     private Keyboard keyboard;
     private boolean isInCredits = false;
     private Animation animation;
+
     public Menu(int row, int col) {
         //this.game = game;
         this.row = row;
         this.col = col;
         this.keyboard = new Keyboard(this);
-        this.animation = new Animation();
-
     }
 
     @Override
@@ -46,99 +44,110 @@ public class Menu implements Grid, KeyboardHandler {
                 break;
 
             case KeyboardEvent.KEY_3:
+
                 if (isInCredits) {
+                    System.out.println("main menu");
                     menu.delete();
                     mainMenu();
                     isInCredits = false;
                 } else {
+                    // animation.setPumped(false);
                     credits();
                     isInCredits = true;
-                }
+                    //}
+                    break;
 
-                break;
+                }
         }
     }
+        @Override
+        public void keyReleased (KeyboardEvent keyboardEvent){
 
-    @Override
-    public void keyReleased(KeyboardEvent keyboardEvent) {
+        }
 
+        public void init () {
+
+            KeyboardEvent enterKey = new KeyboardEvent();
+            enterKey.setKey(KeyboardEvent.KEY_1);
+            enterKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+            keyboard.addEventListener(enterKey);
+
+
+            KeyboardEvent escapeKey = new KeyboardEvent();
+            escapeKey.setKey(KeyboardEvent.KEY_2);
+            escapeKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+            keyboard.addEventListener(escapeKey);
+
+            KeyboardEvent creditsKey = new KeyboardEvent();
+            creditsKey.setKey(KeyboardEvent.KEY_3);
+            creditsKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+            keyboard.addEventListener(creditsKey);
+
+            KeyboardEvent creditsExitKey = new KeyboardEvent();
+            creditsExitKey.setKey(KeyboardEvent.KEY_4);
+            creditsExitKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+            keyboard.addEventListener(creditsExitKey);
+
+            mainMenu();
+
+        }
+
+        private void mainMenu () {
+
+            menu = new Picture(col, row, "resources/git_invaders_logo_4.png");
+            menu.draw();
+            this.animation = new Animation(menu, 6);
+            //animation.draw();
+            // animation.init();
+
+        }
+
+        private void credits () {
+            menu.delete();
+            menu = new Picture(col, row, "resources/git_invaders_credits.png");
+            menu.draw();
+
+        }
+
+        private void gameOver () {
+            menu = new Picture(col, row, "resources/git_invaders_gameOver.png");
+            menu.draw();
+        }
+
+
+        public void delete () {
+            menu.delete();
+        }
+
+        public double getRow () {
+            return row;
+        }
+
+        public double getCol () {
+            return col;
+        }
+
+        public double getWidth () {
+            return col * CELLSIZE;
+        }
+
+        public double getHeight () {
+            return row * CELLSIZE;
+        }
+
+        public void setRow ( int row){
+            this.row = row;
+        }
+
+        public void setCol ( int col){
+            this.col = col;
+        }
+
+        public int getCELLSIZE () {
+            return CELLSIZE;
+        }
+
+        public void setCELLSIZE ( int CELLSIZE){
+            this.CELLSIZE = CELLSIZE;
+        }
     }
-
-    public void init() {
-
-        mainMenu();
-
-        KeyboardEvent enterKey = new KeyboardEvent();
-        enterKey.setKey(KeyboardEvent.KEY_1);
-        enterKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        keyboard.addEventListener(enterKey);
-
-
-        KeyboardEvent escapeKey = new KeyboardEvent();
-        escapeKey.setKey(KeyboardEvent.KEY_2);
-        escapeKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        keyboard.addEventListener(escapeKey);
-
-        KeyboardEvent creditsKey = new KeyboardEvent();
-        creditsKey.setKey(KeyboardEvent.KEY_3);
-        creditsKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        keyboard.addEventListener(creditsKey);
-    }
-
-    private void mainMenu() {
-
-
-       //pictures[0] = new Picture(getWidth() / 2, getWidth() / 2, "")
-        menu = new Picture(col, row, "resources/git_invaders_logo.png");
-        menu.draw();
-    }
-
-    private void credits() {
-        menu.delete();
-        menu = new Picture(col, row, "resources/git_invaders_credits.png");
-        menu.draw();
-
-    }
-
-    private void gameOver() {
-        menu = new Picture(col, row, "resources/git_invaders_gameOver.png");
-        menu.draw();
-    }
-
-
-    public void delete() {
-        menu.delete();
-    }
-
-    public double getRow() {
-        return row;
-    }
-
-    public double getCol() {
-        return col;
-    }
-
-    public double getWidth() {
-        return col * CELLSIZE;
-    }
-
-    public double getHeight() {
-        return row * CELLSIZE;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public void setCol(int col) {
-        this.col = col;
-    }
-
-    public int getCELLSIZE() {
-        return CELLSIZE;
-    }
-
-    public void setCELLSIZE(int CELLSIZE) {
-        this.CELLSIZE = CELLSIZE;
-    }
-}
